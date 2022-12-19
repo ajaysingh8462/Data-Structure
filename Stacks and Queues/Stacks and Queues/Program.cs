01796134bc517d660aefb248da1c13cf681edc89
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-19 11:51:49 +0530

    [Ajay]-pushing 3 value in order of 70,30,56

---
+++ Stacks and Queues/Stacks and Queues/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_and_Queues
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack_Ex stack = new Stack_Ex();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.Display();
+        }
+            
+    }
+}
